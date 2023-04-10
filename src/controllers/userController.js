527b6fca3ba139faf63b5f030863b90d33836aa7
Sagar <sagarmaan2015@gmail.com>
2023-04-10 19:19:00 +0530

    update user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -299,3 +299,246 @@ const getUsers = async function (req, res) {
     }
   };
   
+  
+//======================================== Update User ==========================================//
+
+const updateUser = async function (req, res) {
+    try {
+      let userId = req.params.userId;
+  
+      if (!isValidObjectId(userId))
+        return res
+          .status(400)
+          .send({ status: false, message: "User Id is invalid." });
+  
+      let getUserId = await userModel.findOne({ _id: userId });
+      if (!getUserId)
+        return res
+          .status(404)
+          .send({ status: false, message: "User Id not found." });
+  
+      let data = req.body;
+  
+      let files = req.files;
+  
+      let { fname, lname, email, phone, password, address } = data;
+  
+      if (Object.keys(data).length == 0 && (!files || files.length == 0))
+        return res
+          .status(400)
+          .send({ status: false, message: "At least one field is mendatory." });
+  
+      let updatedData = {};
+  
+      if (fname) {
+        if (!validateName(fname)) {
+          return res
+            .status(400)
+            .send({ status: false, message: "First name must be string." });
+        }
+        if (!fname.trim()) {
+          return res
+            .status(400)
+            .send({ status: false, message: "First name can not be empty." });
+        }
+        fname = fname.trim();
+        updatedData.fname = fname;
+      }
+  
+      if (lname) {
+        if (!validateName(lname)) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Last name must be string." });
+        }
+        if (!lname.trim()) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Last name can not be empty." });
+        }
+        lname = lname.trim();
+        updatedData.lname = lname;
+      }
+  
+      if (email) {
+        if (!validateEmail(email)) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Email id must be proper syntax." });
+        }
+        if (!email.trim()) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Email can not be empty." });
+        }
+  
+        let checkEmailId = await userModel.findOne({ email: email });
+        if (checkEmailId) {
+          return res
+            .status(400)
+            .send({
+              status: false,
+              message:
+                "This Email id is already used ,Please provide another Email Id.",
+            });
+        }
+        email = email.trim();
+        updatedData.email = email;
+      }
+      if (phone) {
+        if (!validateMobileNo(phone)) {
+          return res
+            .status(400)
+            .send({
+              status: false,
+              message: "Mobile number must be Indian format.",
+            });
+        }
+        if (!phone.trim()) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Phone number can not be empty." });
+        }
+        let checkphone = await userModel.findOne({ phone: phone });
+        if (checkphone) {
+          return res
+            .status(400)
+            .send({
+              status: false,
+              message:
+                "This phone number is already used ,Please provide another phone number.",
+            });
+        }
+        phone = phone.trim();
+        updatedData.phone = phone;
+      }
+      if (password) {
+        if (!validatePassword(password)) {
+          return res
+            .status(400)
+            .send({ status: false, messsage: "Please provide valid password." });
+        }
+        if (!password.trim()) {
+          return res
+            .status(400)
+            .send({ status: false, message: "Password can not be empty." });
+        }
+        password = password.trim();
+        let hashing = bcrypt.hashSync("password", 8);
+        updatedData.password = hashing;
+      }
+      if (address) {
+        address = JSON.parse(data.address);
+  
+        if (typeof address != "object") {
+          return res
+            .status(400)
+            .send({ status: false, message: "Address must be Object." });
+        }
+  
+        let { shipping, billing } = address;
+        if (shipping) {
+          if (typeof shipping != "object") {
+            return res
+              .status(400)
+              .send({ status: false, message: "Shipping  must be Object." });
+          }
+  
+          let { street, city, pincode } = shipping;
+          if (street) {
+            if (!street.trim()) {
+              return res
+                .status(400)
+                .send({ status: false, message: "Street  can not be empty." });
+            }
+          }
+          if (city) {
+            if (!validatePlace(city)) {
+              return res
+                .status(400)
+                .send({ status: false, message: "Please enter valid city." });
+            }
+            if (!city.trim()) {
+              return res
+                .status(400)
+                .send({ status: false, message: "City  can not be empty." });
+            }
+          }
+          if (pincode) {
+            if (!validatePincode(pincode)) {
+              return res
+                .status(400)
+                .send({
+                  status: false,
+                  message: "Pincode must be in numbers only.",
+                });
+            }
+          }
+        }
+        if (billing) {
+          if (typeof billing != "object") {
+            return res
+              .status(400)
+              .send({ status: false, message: "Billing  must be Otring." });
+          }
+  
+          let { street, city, pincode } = billing;
+          if (street) {
+            if (!street.trim()) {
+              return res
+                .status(400)
+                .send({ status: false, message: "Street  can not be empty." });
+            }
+          }
+          if (city) {
+            if (!validatePlace(city)) {
+              return res
+                .status(400)
+                .send({ status: false, message: "Please enter valid city." });
+            }
+            if (!city.trim()) {
+              return res
+                .status(400)
+                .send({ status: false, message: "City  can not be empty." });
+            }
+          }
+          if (pincode) {
+            if (!validatePincode(pincode)) {
+              return res
+                .status(400)
+                .send({
+                  status: false,
+                  message: "Pincode must be in numbers only.",
+                });
+            }
+          }
+        }
+        updatedData.address = address;
+      }
+  
+      if (files && files.length > 0) {
+        let uploadFileURL = await uploadFile(files[0]);
+  
+        updatedData.profileImage = uploadFileURL;
+      }
+  
+      let updateUserData = await userModel.findOneAndUpdate(
+        { _id: userId },
+        updatedData,
+        { new: true }
+      );
+  
+      return res
+        .status(200)
+        .send({
+          status: true,
+          message: "User profile updated",
+          data: updateUserData,
+        });
+    } catch (error) {
+      return res.status(500).send({ status: false, message: error.message });
+    }
+  };
+  
+  module.exports = { register, getUsers, userLogin, updateUser };
+  
